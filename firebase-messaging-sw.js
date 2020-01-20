02d249e4ab023ee8bac82f38147f0aab0a135bf0
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2020-01-19 23:29:15 -0500

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
+firebase.initializeApp({
+  authDomain: "tudem-acc4a.firebaseapp.com",
+  projectId: "tudem-acc4a",
+  messagingSenderId: "153126974837",
+  appId: "1:153126974837:web:8cbf120c29ccbbabb04f3c"
+});
+const messaging = firebase.messaging();
